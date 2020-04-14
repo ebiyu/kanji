704d1066c64519bcf425d20b85408b0ee7c61382
海老原祐輔 <ebiyuu1121@gmail.com>
2020-04-14 10:15:50 +0900

    できた

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app dark>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-toolbar-title>カンジア～テル</v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
